f46e4d8ca2cd64bfda52a7843cdfeabc35987090
gavinswan <gavtswan@gmail.com>
2021-02-12 01:32:08 +0000

    added mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breedName = args[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 
-request(url, (error, response, body) => {
-  // console.log('error:', error);
-  // console.log('statusCode:', response && response.statusCode);
-  // console.log('body:', body);
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    // console.log('error:', error);
+    // console.log('statusCode:', response && response.statusCode);
+    // console.log('body:', body);
+    const data = JSON.parse(body);
+    if (error || data.length === 0) {
+      return callback(error, null);
+    }
+    console.log(data);
+    const desc = data[0].description;
+    callback(null, desc);
+  });
+};
+module.exports = { fetchBreedDescription };
+
